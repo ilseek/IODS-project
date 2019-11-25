c76f1237a69df4f2212341bc421de5c0f2261ed0
ilseek <ilse.ekman@uef.fi>
2019-11-25 16:19:55 +0200

    R script

---
+++ create_human.R
@@ -0,0 +1,13 @@
+#create_human.R data
+
+hd <- read.csv("http://s3.amazonaws.com/assets.datacamp.com/production/course_2218/datasets/human_development.csv", stringsAsFactors = F)
+gii <- read.csv("http://s3.amazonaws.com/assets.datacamp.com/production/course_2218/datasets/gender_inequality.csv", stringsAsFactors = F, na.strings = "..")
+
+str(hd)
+dim(hd)
+summary(hd)
+
+str(gii)
+dim(gii)
+summary(gii)
+
