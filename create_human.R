43c3a03d17636d316c68d43dd3a51ad0a5f9d80b
ilseek <ilse.ekman@uef.fi>
2019-12-02 15:15:51 +0200

    Script update

--- create_human.R
+++ create_human.R
@@ -6,5 +6,22 @@ Source of data: http://s3.amazonaws.com/assets.datacamp.com/production/course_22
 
 human <- read.csv("http://s3.amazonaws.com/assets.datacamp.com/production/course_2218/datasets/human1.txt")
 
+names(human)
 str(human)
+dim(human)
+summary(human)
+
+library(stringr)
+str(human$GNI)
+str_replace(human$GNI, pattern=",", replace ="") %>% as.numeric
+
+complete.cases(human)
+data.frame(human[-1], comp = complete.cases(human))
+human_ <- filter(human, complete.cases(human))
+
+tail(human, 10)
+last <- nrow(human) - 7
+human_ <- human[1:last, ]
+rownames(human) <- human$Country
+
 dim(human)
